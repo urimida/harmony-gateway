a4a0943139bb84ab7b5d4650cdad9f431b9f376e
urimida <crystal5k123@gmail.com>
2024-04-21 00:57:39 +0900

    sixth-commit-invite

--- src/kind/FisrtSelection.js
+++ src/kind/FisrtSelection.js
@@ -5,24 +5,12 @@ import Yellowbox from '../images/YellowBox.png';
 import separatorImage from '../images/separatorImage.png';
 
 const FirstSelection = () => {
-    const navigate = useNavigate(); // useNavigate 훅을 사용하여 navigate 함수 가져오기
-
-    const showMessage = (title, description) => {
-        alert(`${title}\n${description}`);
-    };
-
-    const handleElderButtonClick = () => {
-        navigate("/elder-section");
-    };
-
-    const handleYoungButtonClick = () => {
-        navigate("/young-section");
-    };
+    const navigate = useNavigate();
 
     return (
         <div className="first-selection">
             <div className="container">
-                <div className="quote-box" style={{ backgroundImage: `url(${Yellowbox})` }} onClick={handleElderButtonClick}>
+                <div className="quote-box" style={{ backgroundImage: `url(${Yellowbox})` }} onClick={() => navigate('/elder-section')}>
                     <div className="title">
                         The elder
                     </div>
@@ -33,7 +21,7 @@ const FirstSelection = () => {
                 <div className="separator">
                     <img src={separatorImage} alt="Separator" />
                 </div>
-                <div className="quote-box" style={{ backgroundImage: `url(${Yellowbox})` }} onClick={handleYoungButtonClick}>
+                <div className="quote-box" style={{ backgroundImage: `url(${Yellowbox})` }}  onClick={() => navigate('/young-section')}>
                     <div className="title">
                         The young people
                     </div>
