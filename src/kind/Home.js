76e85b05cf0e8b3158b71dd30bf42863ba490c53
urimida <crystal5k123@gmail.com>
2024-06-04 14:26:42 +0900

    Home update5

--- src/kind/Home.js
+++ src/kind/Home.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import '../kind.css';
 import Tabs from "./Tabs";
 import MissionStatusImg from '../images/MissionStatus.png';
 import NewsBox from '../images/NewsBox.png';
-
+import ListofPosts from '../images/ListofPosts.png';
 
 const Home = () => {
+    const [showModal, setShowModal] = useState(false);
+
+    const toggleModal = () => {
+        setShowModal(!showModal);
+    };
 
     return (
         <div className="Home">
@@ -35,25 +40,27 @@ const Home = () => {
                 </div>
             </div>
 
-
             <div className="HomeComponent2">
-                <div className="TodayHarmonyStory">
-                    Today’s Harmony Story
-                </div>
+                <div className="TodayHarmonyStory">Today’s Harmony Story</div>
 
                 <div className="StoryComponent" style={{ backgroundImage: `url(${NewsBox})` }}>
                     <a href="https://n.news.naver.com/article/052/0002042417?sid=102" className="HarmonyStoryTitle">
                         The children who helped the grandfather collecting waste paper.
                     </a>
                 </div>
-
+                <button className="PostButton" onClick={toggleModal}style={{ backgroundImage: `url(${ListofPosts})` }}></button>
             </div>
 
+            <div className={`PostModal ${showModal ? 'show' : ''}`}>
+                <div className="PostContent">
+                    <h2>Posts</h2>
+                    <button onClick={toggleModal}>Close</button>
+                    {/* Add your post content here */}
+                </div>
+            </div>
 
-            <Tabs></Tabs>
+            <Tabs />
         </div>
-
-
     );
 };
 
